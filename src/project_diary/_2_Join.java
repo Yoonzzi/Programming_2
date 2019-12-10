e66308bd534df555762cc752134dec3b4e271c52
Yoonzzi <yoonji@pluto.network>
2019-12-10 13:22:06 +0900

    add join admin

--- src/project_diary/_2_Join.java
+++ src/project_diary/_2_Join.java
@@ -22,10 +22,10 @@ import javax.swing.JTextField;
 public class _2_Join extends GUIManager
 {
 	static String fileSrc = "Login_data.txt";
-	JLabel lbId,lbPw,lbName,lbTitle;
+	JLabel lbId,lbPw,lbName,lbTitle, laAdminNo;
 	JButton bEnter,bCancel;
 	JTextField txtId,txtName;
-	JPasswordField txtPw;
+	JPasswordField txtPw, txtAdminNo;
 
 	_2_Join (){
 		Container contentPane = getContentPane();
@@ -45,6 +45,9 @@ public class _2_Join extends GUIManager
 		bEnter = new JButton();
 		bCancel = new JButton();
 		
+		laAdminNo = new JLabel();
+		txtAdminNo = new JPasswordField();
+		
 		createJLabel(contentPane, lbTitle, "Sign up", 200, 50, 85, 5, f1);
 		createJLabel(contentPane, lbId, "User ID : ", 80, 20, 40, 65, f2);
 		createJLabel(contentPane, lbPw, "User PW : ", 80, 20, 40, 90, f2);
@@ -52,27 +55,42 @@ public class _2_Join extends GUIManager
 		createJTextField(contentPane, txtId, "", 100,20,110,65,f2);
 		createJTextField(contentPane, txtName, "", 100,20,110,115,f2);
 		createJPasswordField(contentPane, txtPw, "", 100,20,110,90,f2);
-		createJButton(contentPane, bCancel, "Cancel", 70, 25, 30, 150, f2);
-		createJButton(contentPane, bEnter, "Enter", 108, 25, 106, 150, f2);
+		createJButton(contentPane, bCancel, "Cancel", 70, 25, 30, 180, f2);
+		createJButton(contentPane, bEnter, "Enter", 120, 25, 120, 180, f2);
 		
+		createJLabel(contentPane, laAdminNo, "Admin No", 80, 20, 40, 140, f2);
+		createJPasswordField(contentPane, txtAdminNo, "", 100, 20, 110, 140, f2);
 		
 		
 		bEnter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String id = txtId.getText();
-				String pw = new String(txtPw.getPassword());
-				String name = txtName.getText();
 				
-				if(_1_MainPage.Login_data.get(id)!=null)
-					JOptionPane.showMessageDialog(null, " Already exist ID ", "Join Error", JOptionPane.ERROR_MESSAGE);
+				if(!txtId.getText().equals("") && txtPw.getPassword().length>0 && txtAdminNo.getPassword().length>0){		
+					String id = txtId.getText();
+					String pw = new String(txtPw.getPassword());
+					String name = txtName.getText();
+					String admin = new String(txtAdminNo.getPassword());
+					String adminNo = "1104";
+					
+					if(_1_MainPage.Login_data.get(id)!=null)
+						JOptionPane.showMessageDialog(null, " Already exist ID ", "Join Error", JOptionPane.ERROR_MESSAGE);
 				
-				else if(_1_MainPage.Login_data.get(id)==null ) {
-					_1_MainPage.Login_data.put(id, new LoginData (id,pw,name));
-					_1_MainPage.saveData();
-					JOptionPane.showMessageDialog(null, " Success join ", "Join! ", JOptionPane.INFORMATION_MESSAGE);
-					dispose();
+					else if(_1_MainPage.Login_data.get(id) == null) {
+							if (admin.equals(adminNo)){
+								_1_MainPage.Login_data.put(id, new LoginData (id,pw,name));
+								_1_MainPage.saveData();
+								JOptionPane.showMessageDialog(null, " Success join ", "Join! ", JOptionPane.INFORMATION_MESSAGE);
+								dispose();
+							}
+							else if (!admin.equals(adminNo)){
+								JOptionPane.showMessageDialog(null, " Wrong AdminNo ", "Join Error ", JOptionPane.INFORMATION_MESSAGE);
+							}
+						}
+
+			}
+				else {
+					JOptionPane.showMessageDialog(null, " Please enter all required fields ", "Join Error", JOptionPane.ERROR_MESSAGE);
 				}
-			
 			}
 		});
 		
@@ -88,7 +106,7 @@ public class _2_Join extends GUIManager
 		int screenY=(int)(screen.getHeight() / 2 - this.getHeight() / 2);
 		setLocation(screenX,screenY);
 		setVisible(true);
-		setResizable(false);
+		//setResizable(false);
 	}
 	
 	public static void main(String[] args) {
